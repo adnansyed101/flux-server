db6601f18223c9590d45bce74b5e488e4a36175d
adnan <sirgeant10@gmail.com>
2024-12-03 23:45:04 +0600

    Connect to db and use routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import movieRoutes from "./routes/movie.route.js";
 
 dotenv.config();
 
@@ -8,10 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+app.use("/api/movies", movieRoutes);
 
 app.listen(5000, () => {
   connectDB();
