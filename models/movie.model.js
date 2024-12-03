784ce83fb7769979199488fb547bf93b22accdd5
adnan <sirgeant10@gmail.com>
2024-12-03 23:44:43 +0600

    Create movie model

---
+++ models/movie.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema(
+  {
+    imgLink: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+export default Movie;
