0f71120e88d18fe2dbb3434627c6698a1a898d9a
adnan <sirgeant10@gmail.com>
2024-12-04 14:13:57 +0600

    Add route to get all movies

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { createMovie } from "../controllers/movie.controller.js";
+import { createMovie, getMovies } from "../controllers/movie.controller.js";
 
 const router = express.Router();
+
+// Get All Products
+router.get("/", getMovies);
 
 // Create new product
 router.post("/", createMovie);
