5fd473d6523e0d717e24426e6c35e1ce415a3dc5
adnan <sirgeant10@gmail.com>
2024-12-07 14:52:22 +0600

    Add favMovie routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import movieRoutes from "./routes/movie.route.js";
+import favMovieRoutes from "./routes/favMovie.route.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/movies", movieRoutes);
+app.use("/api/movies/favourites", favMovieRoutes);
 
 app.listen(5000, () => {
   connectDB();
