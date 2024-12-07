8cef256385396093790da6107a9c892a911473c6
adnan <sirgeant10@gmail.com>
2024-12-07 18:11:45 +0600

    Add new schema

--- models/favMovie.model.js
+++ models/favMovie.model.js
@@ -1,12 +1,44 @@
 import mongoose from "mongoose";
 
-const favMovieSchema = new mongoose.Schema({
-  movie: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: true,
-    ref: "Movie",
+const favMovieSchema = new mongoose.Schema(
+  {
+    imgLink: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const FavMovie = mongoose.model("FavMovie", favMovieSchema);
 
