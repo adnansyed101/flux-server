aea1885de5e77530143053207a678eaa974f49cb
adnan <sirgeant10@gmail.com>
2024-12-08 14:55:04 +0600

    Add PORT variable to server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import movieRoutes from "./routes/movie.route.js";
 import favMovieRoutes from "./routes/favMovie.route.js";
@@ -10,10 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/movies/favourites", favMovieRoutes);
 app.use("/api/movies", movieRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server runnigng at port " + PORT);
 });
