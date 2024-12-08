2c6f86b50efcaabdf9929980832902de2ec504bf
adnan <sirgeant10@gmail.com>
2024-12-08 16:09:35 +0600

    Add connectdb to root folder

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import { connectDB } from "./config/db.js";
 import movieRoutes from "./routes/movie.route.js";
 import favMovieRoutes from "./routes/favMovie.route.js";
@@ -11,11 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello From Movies");
+});
 app.use("/api/movies/favourites", favMovieRoutes);
 app.use("/api/movies", movieRoutes);
 
 app.listen(PORT, () => {
-  connectDB();
   console.log("Server runnigng at port " + PORT);
 });
